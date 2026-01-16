b942a76e2d31e0185600c7a19f7d7b98bc941295
Aflah-Naufal-Haadi <aflahnaufalhaadi218@email.com>
2026-01-17 06:17:11 +0700

    initial commit

---
+++ pemesanan_makanan/app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def pesan(request):
+    return render(request, 'form.html')
+
+def pesanan_berhasil(request):
+    return render(request, 'pesanan_berhasil.html')
